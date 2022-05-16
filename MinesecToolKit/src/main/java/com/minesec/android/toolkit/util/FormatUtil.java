234aeb9422191ba8dbe859185293a9f18ab8db25
eric.song <eric.song@theminesec.com>
2022-05-16 11:35:05 +0800

    Added time utils

---
+++ MinesecToolKit/src/main/java/com/minesec/android/toolkit/util/FormatUtil.java
@@ -0,0 +1,20 @@
+package com.minesec.android.toolkit.util;
+
+import android.text.TextUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class FormatUtil {
+    private String getFormatTransactionDateTime(String pattern,Date date) {
+        if (date == null) {
+            return "";
+        }
+        if (TextUtils.isEmpty(pattern)) {
+            return "";
+        }
+        return new SimpleDateFormat(pattern, Locale.getDefault())
+                .format(date);
+    }
+}

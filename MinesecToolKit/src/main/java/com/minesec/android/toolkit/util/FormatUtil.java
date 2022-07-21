dbf148b6199c99c5e228307039e47afa58655f6c
eric.song <eric.song@theminesec.com>
2022-07-21 15:22:40 +0800

    Updated version to be v1.0.7

--- MinesecToolKit/src/main/java/com/minesec/android/toolkit/util/FormatUtil.java
+++ MinesecToolKit/src/main/java/com/minesec/android/toolkit/util/FormatUtil.java
@@ -1,12 +1,15 @@
 package com.minesec.android.toolkit.util;
 
+import android.annotation.SuppressLint;
 import android.text.TextUtils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class FormatUtil {
+    private final static String DATE_TIME_PATTERN_NO_SECOND = "yyyy/MM/dd,HH:mm";
     public static String getFormatTransactionDateTime(String pattern,Date date) {
         if (date == null) {
             return "";
@@ -16,5 +19,17 @@ public class FormatUtil {
         }
         return new SimpleDateFormat(pattern, Locale.getDefault())
                 .format(date);
+    }
+
+    public static String convertMineSecDataTime(String dateTime) {
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date= simpleDateFormat.parse(dateTime);
+            return getFormatTransactionDateTime(DATE_TIME_PATTERN_NO_SECOND, date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
